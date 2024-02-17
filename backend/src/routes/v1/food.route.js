41850c3955e652ce9fb1b90bfff42d2f046e8f81
xXDMOGXx <83487906+xXDMOGXx@users.noreply.github.com>
2024-02-17 15:15:41 -0600

    Added the module.exports to food.route

--- backend/src/routes/v1/food.route.js
+++ backend/src/routes/v1/food.route.js
@@ -7,3 +7,5 @@ router.route('/').post(foodController.createFood);
 router.route('/').patch(foodController.updateFood);
 router.route('/').get(foodController.getFood);
 router.route('/').delete(foodController.deleteFood);
+
+module.exports = router;
