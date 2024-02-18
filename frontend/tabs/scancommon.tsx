74c8dca27ae6fc1e7207916102d8886b725c5329
Colin Wong <cwongmath@gmail.com>
2024-02-18 03:04:00 -0600

    Fix post request with scan

--- frontend/tabs/scancommon.tsx
+++ frontend/tabs/scancommon.tsx
@@ -2,10 +2,11 @@ import { SERVER_IP, USERID } from "../consts";
 
 export function onCodeScan(code: string) {
     let temp = {
-        "user": {USERID},
-        "barcode": {code},
+        "user": USERID,
+        "barcode": code,
         "calories": 3,
-        "score": 5
+        "score": 5,
+        "name": "Test food"
     }
     fetch(`${SERVER_IP}/v1/food/`, {
       method: "POST", body: JSON.stringify(temp), headers: { "Content-Type": "application/json" }
