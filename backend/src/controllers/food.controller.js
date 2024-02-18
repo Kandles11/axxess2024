ed4e238b935e4fbbf0390324fd916da4cc5df212
xXDMOGXx <83487906+xXDMOGXx@users.noreply.github.com>
2024-02-17 20:05:17 -0600

    Fixed casting error

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -44,7 +44,7 @@ const getFoodByUser = catchAsync(async (req, res) => {
 
   try {
     if (n) {
-      const food = await Food.find({ user: user }).limit(n).exec();
+      const food = await Food.find({ user: user }).limit(parseInt(n)).exec();
       res.status(200).json(food);
     } else {
       const food = await Food.find({ user: user }).exec();
