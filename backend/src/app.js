eb0743e2f005f817e10ccba47e96aa29ecba16ba
xXDMOGXx <83487906+xXDMOGXx@users.noreply.github.com>
2024-02-18 09:01:41 -0600

    remove unnecessary files

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,21 +4,9 @@ const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const compression = require('compression');
 const cors = require('cors');
-const passport = require('passport');
-const httpStatus = require('http-status');
-const config = require('./config/config');
-const morgan = require('./config/morgan');
-const { jwtStrategy } = require('./config/passport');
 const routes = require('./routes/v1');
-const { errorConverter, errorHandler } = require('./middlewares/error');
-const ApiError = require('./utils/ApiError');
 
 const app = express();
-
-if (config.env !== 'test') {
-  app.use(morgan.successHandler);
-  app.use(morgan.errorHandler);
-}
 
 // set security HTTP headers
 app.use(helmet());
@@ -42,13 +30,5 @@ app.options('*', cors());
 
 // v1 api routes
 app.use('/v1', routes);
-
-// send back a 404 error for any unknown api request
-app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
-});
-
-// convert error to ApiError, if needed
-app.use(errorConverter);
 
 module.exports = app;
