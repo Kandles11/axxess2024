daa2899b43eedd269ea533cf95edfe1b5e4bcbb9
Colin Wong <cwongmath@gmail.com>
2024-02-18 03:20:23 -0600

    Randomized temp data

--- frontend/tabs/scancommon.tsx
+++ frontend/tabs/scancommon.tsx
@@ -1,12 +1,29 @@
 import { SERVER_IP, USERID } from "../consts";
 
+function choose(choices: any[]) {
+  var index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
+
+const NAMES = [
+  "Ruffles",
+  "Doritos Cool Ranch",
+  "Pack of Peanuts",
+  "12oz Coca-Cola",
+  "Water Bottle",
+  "BBQ Lays Chips",
+  "Nutella",
+  "Starbucks Canned Coffee",
+  "Nature Valley Granola Bar"
+];
+
 export function onCodeScan(code: string) {
     let temp = {
         "user": USERID,
         "barcode": code,
-        "calories": 3,
-        "score": 5,
-        "name": "Test food"
+        "calories": 50 + Math.floor(Math.random() * 300),
+        "score": 1 + Math.floor(Math.random() * 10),
+        "name": choose(NAMES)
     }
     fetch(`${SERVER_IP}/v1/food/`, {
       method: "POST", body: JSON.stringify(temp), headers: { "Content-Type": "application/json" }
